9aa41a6298ab4724fdace0b6427facba784770ca
Wyesumu <wyesumu@gmail.com>
2020-06-10 12:55:34 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import flask
+import os
+import re
+
+from flask_admin import Admin, AdminIndexView, expose
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.form.upload import FileUploadField, ImageUploadField
+
+app = flask.Flask(__name__)
+db = SQLAlchemy(app)
+
+class Setting(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String())
+    value = db.Column(db.String())
+
+    def __repr__(self):
+        return self.name
+
+class Film(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String())
+    url = db.Column(db.String())
+    
+
+    def __repr__(self):
+        return self.name
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Accept-Ranges', 'bytes')
+    return response
+
+
+def get_chunk(byte1=None, byte2=None, video_name=''):
+    full_path = video_name + '.mp4'
+    file_size = os.stat(full_path).st_size
+    start = 0
+    length = 102400
+
+    if byte1 < file_size:
+        start = byte1
+    if byte2:
+        length = byte2 + 1 - byte1
+    else:
+        length = file_size - start
+
+    with open(full_path, 'rb') as f:
+        f.seek(start)
+        chunk = f.read(length)
+    return chunk, start, length, file_size
+
+
+@app.route('/video')
+def get_file():
+    video_name = flask.request.args.get('video_n')
+    range_header = flask.request.headers.get('Range', None)
+    byte1, byte2 = 0, None
+    if range_header:
+        match = re.search(r'(\d+)-(\d*)', range_header)
+        groups = match.groups()
+
+        if groups[0]:
+            byte1 = int(groups[0])
+        if groups[1]:
+            byte2 = int(groups[1])
+
+    chunk, start, length, file_size = get_chunk(byte1, byte2, video_name)
+    resp = flask.Response(chunk, 206, mimetype='video/mp4',
+                      content_type='video/mp4', direct_passthrough=True)
+    resp.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
+    return resp
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(threaded=True)
